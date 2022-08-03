e5f95ee070c14cf71d00684770e3bbc75892a067
Lakshya Tyagi <lakshya142000@gmail.com>
2022-08-03 18:11:18 +0530

    Add files via upload

---
+++ day 3/task1/Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication4.Models;
+
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IEmployeeRepository emprepo;
+
+        public HomeController(ILogger<HomeController> logger, IEmployeeRepository erep)
+        {
+
+            _logger = logger;
+            emprepo = erep;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult AllEmp()
+        {
+            var model = emprepo.GetAllEmployees();
+            return View(model);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee obj)
+        {
+            emprepo.AddEmployee(obj);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            Employee obj = emprepo.GetEmployeeByID(id);
+            return View(obj);
+        }
+
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Employee obj = emprepo.GetEmployeeByID(id);
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Employee obj)
+        {
+            emprepo.UpdateEmployee(obj);
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            Employee obj = emprepo.GetEmployeeByID(id);
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult DeleteConfirm(int id)
+        {
+            emprepo.DeleteEmployee(id);
+            return RedirectToAction("AllEmp");
+        }
+    }
+}
---
+++ day 3/task1/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace WebApplication4.Models
+{
+    [Table("tblEmployee")]
+    public class Employee
+    {
+        [Key]
+        public int Eid { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public float Salary { get; set; }
+        public string Address { get; set; }
+    }
+}
